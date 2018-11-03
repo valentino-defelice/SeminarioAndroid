bb4fb42eb274d8a7aa954ae8b0ae6bedce6d30db
Valentino <valentinodefelice@gmail.com>
2018-11-03 18:15:41 -0300

    [ADD] Add ListView to display Actors. [IMP] Now, we do the api request in thread.

--- app/src/main/java/com/example/valentino/restapitest/DisplayMovieActivity.java
+++ app/src/main/java/com/example/valentino/restapitest/DisplayMovieActivity.java
@@ -17,8 +17,10 @@ import com.squareup.picasso.Picasso;
 import org.w3c.dom.Text;
 
 import java.io.IOException;
+import java.lang.reflect.Array;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -30,56 +32,77 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class DisplayMovieActivity extends AppCompatActivity {
 
+    ListView listaActores;
+    ArrayList<String> ListActores = new ArrayList<>();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_movie);
          TextView textView = findViewById(R.id.textView);
 
-         TextView actores = findViewById(R.id.listaActores);
+
          ImageView imgPoster = findViewById(R.id.poster);
 
+         ArrayAdapter arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, ListActores);
+         listaActores = findViewById(R.id.ListViewActores);
 
 
          Intent intent = getIntent();
          String Pelicula = intent.getStringExtra(MainActivity.PELICULA);
 
-        getMovie(Pelicula, textView, actores, imgPoster);
+        listaActores.setAdapter(arrayAdapter);
+        getMovie(Pelicula, textView, imgPoster, arrayAdapter);
+
 
     }
 
-    public void getMovie(String Pelicula, final TextView textView, final TextView actores, final ImageView imgPoster) {
-
-        Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl("http://www.omdbapi.com")
-                .addConverterFactory(GsonConverterFactory.create())
-                .build();
-
-        MovieService service = retrofit.create(MovieService.class);
-        Call<Movie> movie = service.searchedMovie(Pelicula, "2dbc1f17");
-
-        movie.enqueue(new Callback<Movie>() {
+    public void getMovie(final String Pelicula, final TextView textView, final ImageView imgPoster, final ArrayAdapter arrayAdapter) {
+        new Thread(new Runnable() {
             @Override
-            public void onResponse(Call<Movie> call, Response<Movie> response) {
-                // response.isSuccessful() &&
-                if (response.body().getResponse().equals("False")) {
-                    textView.setText("¡No la encuentro!");
-                    actores.setText("¿Acaso esa eplicula existe?");
-                } else {
-                    textView.setText(response.body().getTitle());
-                    actores.setText(response.body().getActors());
-                    Picasso.get().load(response.body().getPoster()).into(imgPoster);
-                }
+            public void run() {
+                Retrofit retrofit = new Retrofit.Builder()
+                        .baseUrl("http://www.omdbapi.com")
+                        .addConverterFactory(GsonConverterFactory.create())
+                        .build();
 
+                MovieService service = retrofit.create(MovieService.class);
+                Call<Movie> movie = service.searchedMovie(Pelicula, "2dbc1f17");
+
+
+                movie.enqueue(new Callback<Movie>() {
+                    @Override
+                    public void onResponse(Call<Movie> call, Response<Movie> response) {
+
+                        if (response.body().getResponse().equals("False"))
+                            {
+                                textView.setText("¡No la encuentro!");
+                            }
+                        else
+                            {
+                            textView.setText(response.body().getTitle());
+                            Picasso.get().load(response.body().getPoster()).into(imgPoster);
+
+                            List<String> items = Arrays.asList(response.body().getActors().split("\\s*,\\s*"));
+                            for (String item : items) {
+                                ListActores.add(item);
+                            }
+                            arrayAdapter.notifyDataSetChanged();
+
+                        }
+
+                    }
+
+                    @Override
+                    public void onFailure(Call<Movie> call, Throwable t) {
+                        textView.setText("Error al buscar la pelicula");
+                        Log.e("Movies:", "Error al buscar peli, " + t);
+                    }
+
+                });
             }
+        }).start();
 
-            @Override
-            public void onFailure(Call<Movie> call, Throwable t) {
-                textView.setText("Error al buscar la pelicula");
-                Log.e("Movies:", "Error al buscar peli, " + t);
-            }
-
-        });
     }
 
 }
